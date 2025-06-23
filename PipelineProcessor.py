3679584fa1bea4aa5410f56973caf0d47105b61c
GABOBB <gabbol2002@gmail.com>
2025-06-23 02:04:21 -0600

    se agregan las instrucciones BEQ con labels fucnionales

--- PipelineProcessor.py
+++ PipelineProcessor.py
@@ -4,13 +4,29 @@ class PipelineProcessor:
         self.registers = [0] * 32  # Registros x0 a x31
         self.memory = {}          # Memoria simulada
         self.instructions = []    # Instrucciones en texto
+        self.labels = {}            #diccionario para etiquetas
         self.running = False
 
     def load_program(self, program_text):
-        """Carga un programa en formato de texto"""
-        self.instructions = program_text.strip().split('\n')
+        self.instructions = []
+        self.labels = {}
         self.pc = 0
         self.running = True
+
+        lines = program_text.strip().split('\n')
+
+        # Paso 1: mapa de etiquetas
+        index = 0
+        for line in lines:
+            clean = line.strip()
+            if not clean or clean.startswith("#"):
+                continue
+            if ":" in clean:
+                label = clean.replace(":", "").strip()
+                self.labels[label] = index
+            else:
+                self.instructions.append(clean)
+                index += 1
 
     def step(self):
         """Ejecuta una instrucción"""
@@ -66,6 +82,16 @@ class PipelineProcessor:
                 offset, base = parts[2].replace(')', '').split('(')
                 addr = self.registers[int(base[1:])] + int(offset)
                 self.memory[addr] = self.registers[rs2]
+
+            elif op == "BEQ":
+                rs1 = int(parts[1][1:])
+                rs2 = int(parts[2][1:])
+                label = parts[3]
+                if self.registers[rs1] == self.registers[rs2]:
+                    if label in self.labels:
+                        self.pc = self.labels[label] - 1  # -1 porque el step suma +1 después
+                    else:
+                        print(f"Etiqueta no encontrada: {label}")
 
             else:
                 print(f"Instrucción no soportada: {op}")
